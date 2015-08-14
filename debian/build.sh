19e7f778dd03f6f9ac072d0e7c579d1575f255eb
Luar Roji <luar@scrapinghub.com>
2015-08-14 04:27:16 +0200

    Updated debian building script

--- debian/build.sh
+++ debian/build.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash -x
 
 debuild --no-lintian -us -uc -b
 
-curl -T ../*deb -u$BINTRAY_USER:$BINTRAY_APIKEY
+FILENAME=`basename ../*deb`
+VERSION=`echo $FILENAME | sed "s/logrotate-//;s/.deb$//"`
+
+curl -T ../$FILENAME -u$BINTRAY_USER:$BINTRAY_APIKEY "https://api.bintray.com/content/scrapinghub/logrotate/pool/main/l/logrotate/$FILENAME;bt_package=logrotate;bt_version=$VERSION;deb_distribution=precise;deb_component=main;deb_architecture=amd64;publish=1"
